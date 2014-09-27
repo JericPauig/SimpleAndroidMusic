e7c0535ec4f7836ead53dba5124ce4c98f4c0bb7
JericPauig <jose_pauig3@hotmail.com>
2014-09-27 16:30:05 -0400

    Sorting by artist/title implemented

--- TestApplication/app/src/main/java/com/example/jeric/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/jeric/testapplication/MainActivity.java
@@ -11,24 +11,29 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.MediaController;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements MediaController.MediaPlayerControl {
 
     private ArrayList<Song> songList;
     private ListView songView;
     private MusicService musicService;
     private Intent playIntent;
     private boolean musicBound = false;
+    private MusicController musicController;
+
+    private void setController(){
+        musicController = new MusicController(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,13 +98,39 @@ public class MainActivity extends Activity {
 
         switch (id) {
             case R.id.action_settings:
-                Log.d("Jeric", "Setting Selected");
                 break;
             case R.id.end_play:
-                Log.w("Jeric", "End Selected");
                 stopService(playIntent);
                 musicService = null;
                 System.exit(0);
+                break;
+            case R.id.sort_by_title:
+                songList = new ArrayList<Song>();
+                getSongList();
+                //Sort the songs by title
+                Collections.sort(songList, new Comparator<Song>() {
+                    public int compare(Song a, Song b) {
+                        return a.getTitle().compareTo(b.getTitle());
+                    }
+                });
+                SongAdapter songAdapter = new SongAdapter(this, songList);
+                songView.setAdapter(songAdapter);
+                musicService.setSongs(songList);
+                break;
+            case R.id.sort_by_artist:
+                songList = new ArrayList<Song>();
+                getSongList();
+
+                //Sort the songs by title
+                Collections.sort(songList, new Comparator<Song>() {
+                    public int compare(Song a, Song b) {
+                        return a.getArtist().compareTo(b.getArtist());
+                    }
+                });
+
+                songAdapter = new SongAdapter(this, songList);
+                songView.setAdapter(songAdapter);
+                musicService.setSongs(songList);
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -121,9 +152,15 @@ public class MainActivity extends Activity {
             int titleColumnIndex = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int artistColumnIndex = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
             int idColumnIndex = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            int sizeColumnIndex = musicCursor.getColumnIndex(MediaStore.Audio.Media.SIZE);
 
             //Iterate through each and fill songList
             do {
+                //Most dummy music files are under 500 bytes, so filter them out
+                long songSize = musicCursor.getLong(sizeColumnIndex);
+                if (songSize < 500000){
+                    continue;
+                }
                 long songId = musicCursor.getLong(idColumnIndex);
                 String songTitle = musicCursor.getString(titleColumnIndex);
                 String songArtist = musicCursor.getString(artistColumnIndex);
@@ -137,5 +174,60 @@ public class MainActivity extends Activity {
         stopService(playIntent);
         musicService = null;
         super.onDestroy();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int i) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
---
+++ TestApplication/app/src/main/java/com/example/jeric/testapplication/MusicController.java
@@ -0,0 +1,17 @@
+package com.example.jeric.testapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+/**
+ * Created by Jeric on 27/09/2014.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context context){
+        super(context);
+    }
+
+    public void hide(){}
+}
